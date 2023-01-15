08990c5ed5adeca47c55fd87026c9b4581de8ef4
NCStorey <ncsansom@hotmail.co.uk>
2023-01-15 20:48:48 +0000

    checks and creation of confirm array

--- script.js
+++ script.js
@@ -88,40 +88,93 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-//below chunk of code asks user to input how many charecters they need thier password to be.
-
-function randomIndex (charArray) {
-  Math.floor(math.random)}
-
-
+let passwordLength = 0
+let lowerCaseConfirm = false
+let upperCaseConfirm = false
+let numericConfirm = false
+let specialCharactersConfirm = false
+let randomint = 0
+let password = 0
+let confirmArray = []
 
 // Function to prompt user for password options
 function getPasswordOptions() {
 
-//below chunk of code asks user to input how many charecters they need thier password to be.
-let passwordlength = prompt("please enter desired length of password (10-64)")
+//below is a prompt for password length
+passwordLength = prompt("please enter desired length of password (10-64)")
+
 
 //if statement to ensure a number between 10 and 64 is given
-if (( passwordlength >= 10) && (passwordlength <= 64)){
-  alert("thank you you have chosen " + passwordlength)
-  }
-  else (alert("You have entered a value outside of the parameter 10-64. Please refresh and try again"))
+if (( passwordLength >= 10) && (passwordLength <= 64)){
+  alert("thank you you have chosen a password length of " + passwordLength + " characters.\nNext you will be asked which characters you would like to include. Please confirm to *atleast* one option.")
+  
+  lowerCaseConfirm = confirm("would you like lower case charaters included?")
+  upperCaseConfirm = confirm("would you like upper case charaters included?")
+  numericConfirm = confirm("would you like numerical charaters included?")
+  specialCharactersConfirm = confirm("would you like special charaters included?")}
 
+  else (alert("You have entered a value outside of the parameter 10-64. Page will now refresh") (location.reload()))
+
+  return;
 }
 
 getPasswordOptions()
 
+parseInt(passwordLength)
 
-// Function for getting a random element from an array
-function getRandom(arr) {
+if ((lowerCaseConfirm === false) && (upperCaseConfirm === false) && (numericConfirm === false) && (specialCharactersConfirm === false)){
+  (alert("You need to confirm atleast one option. Page will now refresh") (location.reload()))}
+  else (alert ("Thank you. Please see below for your selections.\n" + 
+  "password length: " + passwordLength + " characters" + 
+  "\nLowercase to be included?: " + lowerCaseConfirm +
+  "\nUppercase to be included?: " + upperCaseConfirm +
+  "\nNumbers to be included?: " + numericConfirm +
+  "\nSpecial Characters to be included?: " + specialCharactersConfirm + 
+  "\nPlease click generate buttom below to see your new password."))
+
+
+
+//CHECKS
+console.log(passwordLength)
+console.log(lowerCaseConfirm)
+console.log(upperCaseConfirm)
+console.log(numericConfirm)
+console.log(specialCharactersConfirm)
+
+
+function createConfirmArray (charaConfirm, arr) {
+  if (charaConfirm === true) {
+    confirmArray = confirmArray.concat(arr)
+  }
 
 }
+
+createConfirmArray (lowerCaseConfirm, lowerCasedCharacters)
+createConfirmArray (upperCaseConfirm, upperCasedCharacters)
+createConfirmArray (numericConfirm, numericCharacters)
+createConfirmArray (specialCharactersConfirm, specialCharacters)
+
+//CHECK
+// console.log(confirmArray)
+
+
+// Function for getting a random element from an array
+
+function getRandom (characonfirm, arr) {
+
+  randomint = Math.floor(Math.random()*arr.length)
+  console.log(randomint)
+
+}
+
+
+confirmArray.forEach(getRandom)
+
 
 // Function to generate password with user input
 function generatePassword() {
 
-  // let = password = "xyz"
-  // return password;
+
 
 
 }
